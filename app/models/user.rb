8b4152c39d8dccd0dbe18a6b4d15de4ae9eea059
Thiago Couto <coutocodes@gmail.com>
2016-03-11 01:45:57 -0500

    View them models

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   	has_many :pins
 
-  	# WT?
+  	# Vincent Magic
     def to_s
   		if self.email
   			return self.email
